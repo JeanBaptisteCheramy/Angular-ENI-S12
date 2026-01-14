572d6ffd4719c0cd925d42d45866cdc3b26f1fde
JeanBaptisteCheramy <jeanbaptiste.cheramy@gmail.com>
2026-01-14 14:17:51 +0100

    mod 6: forms

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {Mod5demo1About} from './mod5/mod5demo1-about/mod5demo1-about';
+import {Mod5demo1} from './mod5/mod5demo1/mod5demo1';
+import {Mod5detail} from './mod5/mod5detail/mod5detail';
+import {AuthGuard} from './mod5/guard/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Mod5demo1 },
+  { path: 'about', component: Mod5demo1About },
+  { path: 'detail/:id', component: Mod5detail, canActivate: [AuthGuard] },
+];
